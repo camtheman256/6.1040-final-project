c70c1b2eb0b3fa6680acb93b0aa7955f4f778113
jasyang <jasyang@mit.edu>
2022-11-15 14:00:07 -0500

    setting up backend boilerplate

---
+++ backend/user/router.ts
@@ -0,0 +1,15 @@
+import type {Request, Response} from 'express';
+import express from 'express';
+
+const router = express.Router();
+
+/**
+ * TODO
+ *
+ * @name GET /api/users
+ *
+ * @return - TODO
+ */
+router.get(
+  '/',
+);
