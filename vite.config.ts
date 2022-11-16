7f0939508a1fd4406f8d3ea3f3b5a63c488ef718
Cameron Kleiman <camtheman256@gmail.com>
2022-11-15 20:18:03 -0500

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const port = env.PORT || 8080;
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": `http://localhost:${port}`,
+      },
+    },
+  };
 });
