aa8a13d01436fbdb7d864dbbb163236bd7ebb360
jasyang <jasyang@mit.edu>
2022-11-22 18:22:42 -0500

    forgot to comment out some stuff

--- backend/space/router.ts
+++ backend/space/router.ts
@@ -2,9 +2,9 @@ import type { Request, Response, NextFunction } from "express";
 import { Types } from "mongoose";
 import express from "express";
 
-import * as spaceMiddleware from "./middleware";
-import SpaceCollection from "./collection";
-import { constructSpaceResponse } from "./util";
+//import * as spaceMiddleware from "./middleware";
+//import SpaceCollection from "./collection";
+//import { constructSpaceResponse } from "./util";
 
 import { assert } from "console";
 
