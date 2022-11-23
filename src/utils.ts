d7b648b9fc4fdf4451b0fc80be469b81aea18407
Piero Orderique <porderiq@mit.edu>
2022-11-23 16:34:51 -0500

    fix lint errors

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export async function get(url: string, bindings: any) {
-    // TODO: write helper code to GET
-};
+  // TODO: write helper code to GET
+}
 export async function post(url: string, body: any) {
-    // TODO: write helper code to POST
-};
+  // TODO: write helper code to POST
+}
