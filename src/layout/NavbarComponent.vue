44c5f008277b42c5ead54d2b59ce912016fae2fa
Cameron Kleiman <camtheman256@gmail.com>
2022-11-23 15:14:54 -0600

    layout navigation with bootstrap

---
+++ src/layout/NavbarComponent.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from "vue-router";
+
+const navLinks = [
+  {
+    route: "/about",
+    label: "About",
+  },
+];
+
+const activeRoute = useRoute();
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">🎯 Requests by Roomba</RouterLink>
+      <button
+        class="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarContent"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarContent">
+        <ul class="navbar-nav me-auto">
+          <li
+            class="nav-item"
+            v-for="{ route, label } of navLinks"
+            :key="route"
+          >
+            <RouterLink
+              class="nav-link"
+              :class="{ active: activeRoute.path === route }"
+              :to="route"
+              >{{ label }}</RouterLink
+            >
+          </li>
+        </ul>
+        <!-- TODO: replace this button with our Google Sign in button
+          also need to handle logged-in state here -->
+        <button class="btn btn-outline-light">Sign In</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: var(--theme-color);
+}
+</style>
