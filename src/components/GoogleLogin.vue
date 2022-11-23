6ebce4f10888cf8ea0b5bc0da5b34e66b5764480
Piero Orderique <porderiq@mit.edu>
2022-11-23 17:20:00 -0500

    temporary post solution

--- src/components/GoogleLogin.vue
+++ src/components/GoogleLogin.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
+import { get, post } from "../utils";
 const callback = (response: Response) => {
   // This callback will be triggered when the user selects or login to
   // his Google account from the popup
   console.log("Handle the response", response);
+  get("/api").then((r) => console.log(r));
+  post("/api/users/session/token-auth", response);
 };
 </script>
 
