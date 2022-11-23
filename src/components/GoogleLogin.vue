e93155770fdcccea2fa6120569b5fccd5ecd9e4a
Piero Orderique <porderiq@mit.edu>
2022-11-23 17:51:26 -0500

    remove unneccessary comments

--- src/components/GoogleLogin.vue
+++ src/components/GoogleLogin.vue
@@ -1,13 +1,11 @@
 <script lang="ts" setup>
-import { get, post } from "../utils";
+import { post } from "../utils";
 interface IGetUserAuthInfoResponse extends Response {
-  credential: string,
+  credential: string;
 }
 const callback = (response: IGetUserAuthInfoResponse) => {
   // This callback will be triggered when the user selects or login to
   // his Google account from the popup
-  console.log("Handle the response", response);
-  // get("/api").then((r) => console.log(r));
   post("/api/users/session/token-auth", {
     ...response,
     token: response.credential,
