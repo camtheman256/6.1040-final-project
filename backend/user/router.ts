dcb71c17ef4ac498682977498c1b86dce13edb16
Cameron Kleiman <camtheman256@gmail.com>
2022-11-23 17:40:22 -0600

    fix response code to match openapi spec

--- backend/user/router.ts
+++ backend/user/router.ts
@@ -31,7 +31,7 @@ router.post(
     const email: string | undefined = payload.email;
     const userDoc = await userMiddleware.createUserFromGapiAuth(payload);
     req.session.userId = userDoc.gapiUserId;
-    res.status(201).json({
+    res.status(200).json({
       message: "You have signed in successfully.",
     });
   }
