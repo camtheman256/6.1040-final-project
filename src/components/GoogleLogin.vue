c43039c5a8886665eef3cb30258c421ab6a856f2
Piero Orderique <porderiq@mit.edu>
2022-11-23 17:50:28 -0500

    working backend call for sign in

--- src/components/GoogleLogin.vue
+++ src/components/GoogleLogin.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
 import { get, post } from "../utils";
-const callback = (response: Response) => {
+interface IGetUserAuthInfoResponse extends Response {
+  credential: string,
+}
+const callback = (response: IGetUserAuthInfoResponse) => {
   // This callback will be triggered when the user selects or login to
   // his Google account from the popup
   console.log("Handle the response", response);
-  get("/api").then((r) => console.log(r));
-  post("/api/users/session/token-auth", response);
+  // get("/api").then((r) => console.log(r));
+  post("/api/users/session/token-auth", {
+    ...response,
+    token: response.credential,
+  });
 };
 </script>
 
