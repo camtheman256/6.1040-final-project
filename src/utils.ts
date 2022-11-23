77ecbad554276b820324a90a536ba0a52fb23ca6
Piero Orderique <porderiq@mit.edu>
2022-11-23 17:20:00 -0500

    temporary post solution

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,14 @@
-export async function get(url: string, bindings: any) {
-  // TODO: write helper code to GET
+export async function get(url: string) {
+  console.log("GET url provided:", url);
+  return fetch(url).then(async (r) => r.json());
 }
-export async function post(url: string, body: any) {
-  // TODO: write helper code to POST
+export async function post(url: string, body: object) {
+  const options = {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+    // credentials: "same-origin", // Sends express-session credentials with request
+  };
+  await fetch(url, options);
+  console.log("POST sent to url:", url);
 }
