e2598a11222324c7814441eeef59312834e62e32
Derek <dschaadt@mit.ed>
2022-11-29 19:38:05 -0500

    Added schema and routes for Requests and Replies

---
+++ backend/reply/router.ts
@@ -0,0 +1,11 @@
+import type { Request, Response, NextFunction } from "express";
+import { Types } from "mongoose";
+import express from "express";
+
+//import * as replyMiddleware from "./middleware";
+//import ReplyCollection from "./collection";
+//import { constructReplyResponse } from "./util";
+
+import { assert } from "console";
+
+const router = express.Router();
---
+++ backend/request/router.ts
@@ -0,0 +1,11 @@
+import type { Request, Response, NextFunction } from "express";
+import { Types } from "mongoose";
+import express from "express";
+
+//import * as requestMiddleware from "./middleware";
+//import RequestCollection from "./collection";
+//import { constructRequestResponse } from "./util";
+
+import { assert } from "console";
+
+const router = express.Router();
