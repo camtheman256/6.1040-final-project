b74bdc340cf7cfecac5208b947f6c8856d073543
Piero Orderique <porderiq@mit.edu>
2022-12-04 09:45:24 -0500

    Add Space page

---
+++ src/components/CreateRequestForm.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+const onSubmit = () => {
+  console.log("form submitted!");
+};
+</script>
+<template>
+  <section class="bg bg-dark text-white">
+    <h4 class="bottom-buffer">Create Request</h4>
+    <form class="mb-3 smallWidth">
+      <div class="input-group bottom-buffer">
+        <div class="btn static btn-outline-secondary" type="button">Name</div>
+        <input
+          type="text"
+          class="form-control"
+          ref="searchbox"
+          placeholder="Request Name"
+        />
+      </div>
+      <div class="input-group bottom-buffer">
+        <div class="btn static btn-outline-secondary" type="button">
+          Description
+        </div>
+        <input
+          type="text"
+          class="form-control"
+          ref="searchbox"
+          placeholder="description"
+        />
+      </div>
+      <button class="btn btn-outline-success" type="submit" @click="onSubmit">
+        Create ➕
+      </button>
+    </form>
+  </section>
+</template>
+
+<style scoped>
+.smallWidth {
+  width: 100%;
+  min-width: 15em;
+}
+.static {
+  pointer-events: none;
+}
+
+.bg {
+  width: 400px;
+  width: 100%;
+  padding: 2rem;
+  border-radius: 30px;
+  box-shadow: -7px -7px rgb(141, 141, 141);
+}
+
+.bottom-buffer {
+  margin-bottom: 2rem;
+}
+</style>
---
+++ src/components/RequestInfo.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+// TODO(porderiq): Add type of request.
+const props = defineProps(["request"]);
+const onCardClick = () =>
+  // TODO(porderiq): redirect to correct view.
+  console.log("Go to page for this card:", props.request);
+
+const requestStatus = (): string => {
+  if (props.request?.resolved) return "Resolved";
+  if (props.request?.inProcess) return "In Progress";
+  return "Not Addressed";
+};
+
+const statusStyle = {
+  "bg-secondary": requestStatus() === "Not Addressed",
+  "bg-warning": requestStatus() === "In Progress",
+  "bg-success": requestStatus() === "Resolved",
+};
+</script>
+
+<template>
+  <div class="card text-white bg-dark" @click="onCardClick">
+    <div class="card-body">
+      <h5 class="card-title spanned">
+        {{ props.request.title }}
+        <span class="text-muted">
+          {{ props.request.upvotingUsers.length }}
+          <a href="#" class="btn btn-sm btn-primary">👍</a>
+        </span>
+      </h5>
+      <h6 class="card-subtitle mb-2 text-muted">{{ props.request.space }}</h6>
+      <p class="card-text">
+        {{ props.request.textContent }}
+      </p>
+      <p class="emphasized">Created {{ props.request.dateCreated }}</p>
+      <div class="btn status text-white" :class="statusStyle">
+        Status: {{ requestStatus() }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.spanned {
+  display: flex;
+  justify-content: space-between;
+}
+.emphasized {
+  font-style: italic;
+  color: rgb(142, 142, 142);
+}
+.status {
+  pointer-events: none;
+}
+</style>
