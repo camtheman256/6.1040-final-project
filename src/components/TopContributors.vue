e02ba734196604f5b41488c686d888c7a2917721
Cameron Kleiman <camtheman256@gmail.com>
2022-12-11 09:55:20 -0500

    crown for local legend

--- src/components/TopContributors.vue
+++ src/components/TopContributors.vue
@@ -18,6 +18,7 @@ onMounted(async () => {
   <h4>Top Check-ins:</h4>
   <ol>
     <li v-for="(rank, idx) of topCheckIns" :key="idx" class="mb-2">
+      <span class="me-2">{{ idx === 0 ? " 👑" : "" }}</span>
       <UserProfile :user="rank.user" class="d-inline-flex align-middle me-2" />
       <span class="badge text-bg-danger"
         >{{ rank.count }} check-in{{ rank.count !== 1 ? "s" : "" }}</span
