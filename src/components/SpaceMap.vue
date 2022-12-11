874d34671599cfcc68430c2d13414a064f6f53f4
Cameron Kleiman <camtheman256@gmail.com>
2022-12-11 09:42:43 -0500

    add local legends to map

--- src/components/SpaceMap.vue
+++ src/components/SpaceMap.vue
@@ -4,14 +4,14 @@ import L, { FeatureGroup, Icon, LayerGroup, Map } from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerIconRetina from "leaflet/dist/images/marker-icon-2x.png";
 import "leaflet/dist/leaflet.css";
-import { onMounted, ref, watch } from "vue";
+import { onMounted, ref, watch, shallowRef } from "vue";
 import { useRouter } from "vue-router";
 import { useCheckInStore } from "@/stores/checkin";
 import { useUserStore } from "@/stores/user";
 
 const props = defineProps<{ spaces: SpaceResponse[] }>();
-const markerLayer = ref<FeatureGroup>(L.featureGroup());
-const map = ref<Map>();
+const markerLayer = shallowRef<FeatureGroup>(L.featureGroup());
+const map = shallowRef<Map>();
 const router = useRouter();
 const checkInStore = useCheckInStore();
 const userStore = useUserStore();
@@ -54,35 +54,70 @@ watch(
 );
 
 function createSpacePopup(space: SpaceResponse): HTMLElement {
+  const container = L.DomUtil.create("div");
   const btnsContainer = L.DomUtil.create("div", "btn-group");
 
-  const visitBtn = L.DomUtil.create("button", "btn btn-primary");
+  const visitBtn = L.DomUtil.create("button", "btn btn-primary", btnsContainer);
   visitBtn.innerText = "Visit Space";
   visitBtn.onclick = () => router.push(`/space/${space.place_id}`);
-  btnsContainer.append(visitBtn);
 
   if (!checkInStore.today && userStore.user) {
-    const checkInBtn = L.DomUtil.create("button", "btn btn-success");
+    const checkInBtn = L.DomUtil.create(
+      "button",
+      "btn btn-success",
+      btnsContainer
+    );
     checkInBtn.innerText = "✅ Check In";
     checkInBtn.onclick = () => {
       checkInStore.checkIn(space.place_id);
       window.alert("Checked in!");
     };
-    btnsContainer.append(checkInBtn);
   }
 
-  const title = L.DomUtil.create("h5");
+  const title = L.DomUtil.create("h5", "", container);
   title.innerText = space.name;
 
-  const container = L.DomUtil.create("div");
-  container.append(title);
+  if (space.totalCheckInCount !== undefined) {
+    const checkinsTotal = L.DomUtil.create("h6", "", container);
+    checkinsTotal.innerText = `${space.totalCheckInCount} total check-in${
+      space.totalCheckInCount !== 1 ? "s" : ""
+    }`;
+  }
+
+  if (space.localLegend !== undefined) {
+    const legendContainer = L.DomUtil.create(
+      "div",
+      "d-flex gap-2 align-items-center mb-1",
+      container
+    );
+    const legendImage = L.DomUtil.create(
+      "img",
+      "rounded-circle",
+      legendContainer
+    );
+    legendImage.src = space.localLegend.imageUrl;
+    legendImage.height = 25;
+    const legendName = L.DomUtil.create("span", "", legendContainer);
+    legendName.innerText =
+      "👑 " +
+      (userStore.user?.gapiUserId === space.localLegend.gapiUserId
+        ? "You"
+        : space.localLegend.name) +
+      ` (${space.localLegendCount} check-in${
+        space.localLegendCount !== 1 ? "s" : ""
+      })`;
+  }
+
   if (
     checkInStore.today &&
     checkInStore.today.space.place_id == space.place_id
   ) {
-    const checkedInBadge = L.DomUtil.create("div", "badge bg-success me-3");
+    const checkedInBadge = L.DomUtil.create(
+      "div",
+      "badge bg-success me-3",
+      container
+    );
     checkedInBadge.innerText = "Checked in here";
-    container.append(checkedInBadge);
   }
   container.append(btnsContainer);
   return container;
